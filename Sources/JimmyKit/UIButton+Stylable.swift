12f8b2aeb2bc40772d7144f6aaf70b649524e11c
Jimmy <joony300@gmail.com>
2023-08-30 22:29:37 +0900

    [Fix] UIButton Add @discardableResult
    실수로 안넣었네요

--- Sources/JimmyKit/UIButton+Stylable.swift
+++ Sources/JimmyKit/UIButton+Stylable.swift
@@ -13,6 +13,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's background color with Highlight Color enabled.
     /// - Parameter color: The background color to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func baseBackgroundColor(_ color: UIColor) -> Self {
         self.configuration?.baseBackgroundColor = color
         return self
@@ -21,6 +22,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's foreground color.
     /// - Parameter color: The foreground color to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func baseForegroundColor(_ color: UIColor) -> Self {
         self.configuration?.baseForegroundColor = color
         return self
@@ -30,6 +32,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's title.
     /// - Parameter text: The title text to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func title(_ text: String) -> Self {
         self.configuration?.title = text
         return self
@@ -39,6 +42,7 @@ extension Stylable where Self: UIButton {
     /// - Parameter text: The title text to be set.
     /// - Parameter state: The state to which the title should be applied.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func title(_ text: String, for state: UIControl.State) -> Self {
         self.setTitle(text, for: state)
         return self
@@ -50,6 +54,7 @@ extension Stylable where Self: UIButton {
     ///   - size: The size of the font to be set.
     ///   - weight: The weight of the font to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func titleWithFont(title: String, size: CGFloat, weight: UIFont.Weight) -> Self {
         var titleAttr = AttributedString(title)
         titleAttr.font = .systemFont(ofSize: size, weight: weight)
@@ -60,6 +65,7 @@ extension Stylable where Self: UIButton {
     /// Set the alignment of the button's title.
     /// - Parameter alignment: The alignment to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func titleAlignment(_ alignment: UIButton.Configuration.TitleAlignment) -> Self {
         self.configuration?.titleAlignment = alignment
         return self
@@ -69,6 +75,7 @@ extension Stylable where Self: UIButton {
     /// Set the padding of the button's title.
     /// - Parameter padding: The padding to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func titlePadding(_ padding: CGFloat) -> Self {
         self.configuration?.titlePadding = padding
         return self
@@ -77,6 +84,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's subtitle.
     /// - Parameter text: The subtitle text to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func subtitle(_ text: String) -> Self {
         self.configuration?.subtitle = text
         return self
@@ -88,6 +96,7 @@ extension Stylable where Self: UIButton {
     ///   - size: The size of the font to be set.
     ///   - weight: The weight of the font to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func subtitleWithFont(title: String, size: CGFloat, weight: UIFont.Weight) -> Self {
         var titleAttr = AttributedString(title)
         titleAttr.font = .systemFont(ofSize: size, weight: weight)
@@ -98,6 +107,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's image.
     /// - Parameter image: The image to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func image(_ image: UIImage?) -> Self {
         self.configuration?.image = image
         return self
@@ -106,6 +116,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's corner style.
     /// - Parameter cornerStyle: The corner style to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func cornerStyle(_ cornerStyle: UIButton.Configuration.CornerStyle) -> Self {
         self.configuration?.cornerStyle = cornerStyle
         return self
@@ -114,6 +125,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's border color.
     /// - Parameter color: The color to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func borderColor(_ color: UIColor) -> Self {
         self.configuration?.background.strokeColor = color
         return self
@@ -122,6 +134,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's border width.
     /// - Parameter width: The width to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func borderWidth(_ width: CGFloat) -> Self {
         self.configuration?.background.strokeWidth = width
         return self
@@ -130,6 +143,7 @@ extension Stylable where Self: UIButton {
     /// Set the button's corner radius.
     /// - Parameter radius: The radius to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func cornerRadius(_ radius: CGFloat) -> Self {
         self.configuration?.background.cornerRadius = radius
         return self
@@ -138,6 +152,7 @@ extension Stylable where Self: UIButton {
     /// Set the padding of the button's image.
     /// - Parameter padding: The padding to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func imagePadding(_ padding: CGFloat) -> Self {
         self.configuration?.imagePadding = padding
         return self
@@ -146,6 +161,7 @@ extension Stylable where Self: UIButton {
     /// Set the placement of the button's image.
     /// - Parameter placement: The placement to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func imagePlacement(_ placement: NSDirectionalRectEdge) -> Self {
         self.configuration?.imagePlacement = placement
         return self
@@ -154,6 +170,7 @@ extension Stylable where Self: UIButton {
     /// Set the content insets of the button.
     /// - Parameter insets: The insets to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func contentInsets(_ insets: NSDirectionalEdgeInsets) -> Self {
         self.configuration?.contentInsets = insets
         return self
@@ -162,6 +179,7 @@ extension Stylable where Self: UIButton {
     /// Set the size of the button.
     /// - Parameter size: The size to be set.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func buttonSize(_ size: UIButton.Configuration.Size) -> Self {
         self.configuration?.buttonSize = size
         return self
@@ -170,6 +188,7 @@ extension Stylable where Self: UIButton {
     /// Set whether the button shows an activity indicator.
     /// - Parameter bool: `true` if the button should show an activity indicator, `false` otherwise.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func showsActivityIndicator(_ bool: Bool) -> Self {
         self.configuration?.showsActivityIndicator = bool
         return self
@@ -178,6 +197,7 @@ extension Stylable where Self: UIButton {
     /// Add a configuration update handler to the button.
     /// - Parameter action: The configuration update handler to be added.
     /// - Returns: The `Self` instance for function chaining.
+    @discardableResult
     public func addConfigurationUpdateHandler(_ action: @escaping UIButton.ConfigurationUpdateHandler) -> Self {
         self.configurationUpdateHandler = action
         return self
