4982c79b00feb07c9f77820cca130c23fc233cad
Jimmy <joony300@gmail.com>
2023-11-13 00:56:34 +0900

    [Fix] AddAction 로직 수정

--- Sources/JimmyKit/DynamicMemberLookup/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by 정준영 on 11/13/23.
-//
-
-import Foundation
---
+++ Sources/JimmyKit/DynamicMemberLookup/UIControl + Builder.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by 정준영 on 11/13/23.
+//
+
+import UIKit
+
+extension Builder where BaseObject: UIControl {
+    
+    @available(iOS 14.0, *)
+    var addAction: (@escaping () -> (), UIControl.Event) -> Self {
+        { [build = _build] _action, event in
+            let identifier = UIAction.Identifier(String(describing: event.rawValue))
+            let action = UIAction(identifier: identifier) { _ in
+                _action()
+            }
+            return Builder {
+                let object = build()
+                object.removeAction(identifiedBy: identifier, for: event)
+                object.addAction(action, for: event)
+                return object
+            }
+        }
+    }
+}
